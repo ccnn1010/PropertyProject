b7c68758f453ed96988a8303fb0f39cc3e00377d
Caterina <ccnn1010@gmail.com>
2021-01-02 01:21:47 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/* $(window).scroll(function(){
+    $('nav').toggelClass('scrolled', $(this).scrollTop() > 200);
+});
+ */
+/* 200 represents how many pixels down */
+
+/* 
+ $(window).scroll(function(){
+    $('nav').toggelClass('scrolled', $(this).scrollTop() > 700);
+     
+    });  */
+
+    $(document).ready(function() {
+        $(window).scroll(function() {
+           if($(this).scrollTop() > 900) { 
+               $('.navbar').addClass('scrolled');
+           } else {
+               $('.navbar').removeClass('scrolled');
+           }
+        });
+    });
